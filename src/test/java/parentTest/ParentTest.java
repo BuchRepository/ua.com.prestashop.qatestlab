fa7b012401055b3401d306051d48d7e3f56e66b9
sergiy_buchkivskyi <Kraftwerke25>
2019-07-04 18:23:31 +0300

    020407

--- src/test/java/parentTest/ParentTest.java
+++ src/test/java/parentTest/ParentTest.java
@@ -1,4 +1,48 @@
 package parentTest;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.*;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class ParentTest {
+    protected WebDriver webDriver;
+    protected LoginPage loginPage;
+    protected HomePage homePage;
+
+
+    @Before
+    public  void before(){
+        File file = new File("./src/newdrivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        loginPage= new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
+
+    }
+
+    @After
+    public void  after(){
+        webDriver.quit();
+    }
+
+    public void checkExpectedResult(String message, boolean expectedResult, boolean actualResult){
+        Assert.assertEquals(message, expectedResult, actualResult);
+        //webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed()
+    }
+
+//    public void checkExpectedResult (String message, boolean actualResult){
+////        checkExpectedResult();
+////    }
+
 }
